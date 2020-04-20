0f8351a6c408fe3bf386d9a9e89dd9b84a7782cd
Heverson Damasceno <heversondamasceno@gmail.com>
2020-04-20 10:20:32 -0300

    Desafio concluído

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
-
+import React,{useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response =>{
+      setProjects(response.data); 
+    })
+  },[]);
+
+  async function handleAddRepository(event) {
+    event.preventDefault();
+    const response = await api.post('repositories',{
+      title: 'Gostack FrontEnd',
+      url: 'https://github.com/Heverson/desafio-conceitos-reactjs',
+      techs: ['ReactJS', 'NodeJs']
+    });
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    if(response.status === 204)
+      setProjects(projects.filter((item) => item.id !== id));
+    else 
+      alert('Ocorreu um erro ao deletar o projeto \nPor favor, tente novamente mais tarde.') 
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {projects.map((item) => (
+            <li key={item.id}>
+              <span>{item.title}</span>
+              <button onClick={()=>handleRemoveRepository(item.id)}>Remover</button></li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
